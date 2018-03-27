c5023a4a9519c933c83319244931ba23af36566a
Time Lord <rizkiduwinanto@gmail.com>
2018-03-27 23:52:15 +0700

    Shader belum kelar

--- Car2.cpp
+++ Car2.cpp
@@ -1,78 +1,120 @@
-#include <GL/gl.h>
-#include <GL/glu.h>
+#include <GL/glew.h>
 #include <GL/glut.h>
-#include <unistd.h>
+#include <iostream>
+#include <string>
 
-#define ESCAPE 27
+std::string vertexShader = "#version 430\n"
+                           "in vec3 pos;"
+                           "void main() {"
+                           "gl_Position = vec4(pos, 1);"
+                           "}";
 
-int window;
-float rotation_tri = 0.0f;
-float rotation_quad = 0.0f;
+std::string fragmentShader = "#version 430\n"
+                             "void main() {"
+                             "gl_FragColor = vec4(0, 1, 0, 0);"
+                             "}";
 
-void initGL(int width, int height){
-  glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-  glClearDepth(1.0);
-  glDepthFunc(GL_LESS);
-  glEnable(GL_DEPTH_TEST);
-  glShadeModel(GL_SMOOTH);
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  gluPerspective(45.0f, (GLfloat) width / (GLfloat) height, 0.1f, 100.0f);
-  glMatrixMode(GL_MODELVIEW);
+
+GLuint compileShaders(std::string shader, GLenum type){
+    const char* shaderCode = shader.c_str();
+    GLuint shaderId = glCreateShader(type);
+    if (shaderId == 0) {
+        std::cout << "Error creating shaders";
+        return 0;
+    }
+    glShaderSource(shaderId, 1, &shaderCode, NULL);
+    glCompileShader(shaderId);
+    GLint compileStatus;
+    glGetShaderiv(shaderId, GL_COMPILE_STATUS, &compileStatus);
+    if (!compileStatus) {
+        int length;
+        glGetShaderiv(shaderId, GL_INFO_LOG_LENGTH, &length);
+        char* cMessage = new char[length];
+        glGetShaderInfoLog(shaderId, length, &length, cMessage);
+        std::cout << "Cannot Compile Shader: " << cMessage;
+        delete[] cMessage;
+        glDeleteShader(shaderId);
+        return 0;
+    }
+
+    return shaderId;
 }
 
-void resizeGLScene(int width, int height){
-  if (height == 0) {
-    height = 1;
-  }
-  glViewport(0, 0, width, height);
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  gluPerspective(45.0f, (GLfloat) width / (GLfloat) height, 0.1f, 100.0f);
-  glMatrixMode(GL_MODELVIEW);
+GLuint linkProgram(GLuint vertexShaderId, GLuint fragmentShaderId){
+    GLuint programId = glCreateProgram();
+    if (programId == 0) {
+        std::cout << "Error Creating Shader Program";
+        return 0;
+    }
+    glAttachShader(programId, vertexShaderId);
+    glAttachShader(programId, fragmentShaderId);
+    glLinkProgram(programId);
+    GLint linkStatus;
+    glGetProgramiv(programId, GL_LINK_STATUS, &linkStatus);
+    if (!linkStatus) {
+        std::cout << "Error Linking program";
+        glDetachShader(programId, vertexShaderId);
+        glDetachShader(programId, fragmentShaderId);
+        glDeleteProgram(programId);
+        return 0;
+    }
+    return programId;
 }
 
-void drawWheel(void) {
+GLuint loadDataInBuffers(){
+    GLfloat vertices[] =
+		{
+			-0.3, -0.3, 0,
+			0.3, -0.3, 0,
+			0.3, 0.3, 0,
+			-0.3, 0.3, 0
+    };
+
+		GLuint elements[] =
+		{
+    	0, 1, 2,
+    	2, 3, 0
+		};
+
+    GLuint vboId;
+    glGenBuffers(1, &vboId);
+    glBindBuffer(GL_ARRAY_BUFFER, vboId);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    return vboId;
 }
 
-void drawCar(){
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-  glLoadIdentity();
-  glTranslatef(-5.0f, 0.0f, -20.0f);
-  glBegin(GL_POLYGON);
-  glColor3f(0.0f, 1.0f, 0.0f);
-  glVertex3f(9.0f, 1.0f, 0.0f);
-  glVertex3f(9.0f, 2.0f, 0.0f);
-  glVertex3f(7.0f, 2.0f, 0.0f);
-  glVertex3f(5.0f, 3.0f, 0.0f);
-  glVertex3f(3.0f, 3.0f, 0.0f);
-  glVertex3f(2.0f, 2.0f, 0.0f);
-  glVertex3f(1.0f, 2.0f, 0.0f);
-  glVertex3f(1.0f, 1.0f, 0.0f);
-  glColor3f(1.0f, 1.0f, 1.0f);
-  glEnd();
-  drawWheel();
-  glutSwapBuffers();
+void init(){
+    glClearColor(0, 0, 0, 0);
+    GLuint vboId = loadDataInBuffers();
+    GLuint vShaderId = compileShaders(vertexShader, GL_VERTEX_SHADER);
+    GLuint fShaderId = compileShaders(fragmentShader, GL_FRAGMENT_SHADER);
+    GLuint programId = linkProgram(vShaderId, fShaderId);
+    GLuint posAttributePosition = glGetAttribLocation(programId, "pos");
+    GLuint vaoId;
+    glGenVertexArrays(1, &vaoId);
+    glBindVertexArray(vaoId);
+    glBindBuffer(GL_ARRAY_BUFFER, vboId);
+    glVertexAttribPointer(posAttributePosition, 3, GL_FLOAT, false, 0, 0);
+    glEnableVertexAttribArray(posAttributePosition);
+    glUseProgram(programId);
 }
 
-void keyPressed(unsigned char key, int x, int y){
-  usleep(100);
-  if (key == ESCAPE){
-    glutDestroyWindow(window);
-  }
-  exit(0);
+void display(){
+    glClear(GL_COLOR_BUFFER_BIT);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glutSwapBuffers();
 }
 
-int main(int argc, char **argv){
-  glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH);
-  glutInitWindowSize(640, 480);
-  glutInitWindowPosition(0, 0);
-  window = glutCreateWindow("Tugas Grafika 9");
-  glutDisplayFunc(&drawCar);
-  glutIdleFunc(&drawCar);
-  glutReshapeFunc(&resizeGLScene);
-  glutKeyboardFunc(&keyPressed);
-  glutMainLoop();
-  return 1;
+int main(int argc, char** argv){
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+		glutInitWindowSize(640, 480);
+	  glutInitWindowPosition(0, 0);
+    glutCreateWindow("Tugas Grafika 9");
+    glewInit();
+    init();
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;
 }
--- makefile
+++ makefile
@@ -1,5 +1,6 @@
-make :
+make:
 	g++ Car1.cpp -o grafika9 -lGL -lGLU -lglut -Wall
+	g++ Car2.cpp -o grafika9 -lGL -lGLU -lglut -Wall -lGLEW
 
 run :
 	./grafika9
